aeb6ece8cd6b024d4bf1c4aeb7291279cc4774e4
anisite <infologique@gmail.com>
2020-03-22 15:51:56 -0400

    Ajout d'un message de confirmation

--- ReceivePDF/Controllers/PdfController.cs
+++ ReceivePDF/Controllers/PdfController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -22,16 +23,31 @@ namespace ReceivePDF.Controllers
         }
 
         [HttpPost]
-        public async Task<ActionResult<string>> PostAsync()
+        public async Task<IActionResult> PostAsync()
         {
             string pdf;
             using (StreamReader reader = new StreamReader(Request.Body, Encoding.UTF8))
             {
                 pdf = await reader.ReadToEndAsync();
             }
-              
 
-            return "Good";
+            //Response.Headers.Add("Content-Type", "application/vnd.fdf");
+            //Response.Headers.Add("Content-disposition", "inline");
+          string t = @"%FDF-1.2
+                        %âãÏÓ
+                        1 0 obj
+                        <<
+                        /FDF
+                            <<
+                            /Status(Merci d'avoir soumis votre demande d'aide.\nNous traiterons votre demande le plus rapidement possible.)
+                            >>
+                        >>
+                        endobj
+                        trailer
+                        <</Root 1 0 R>>
+                        %%EOF";
+
+            return Content(t, "application/vnd.fdf");
         }
     }
 }
