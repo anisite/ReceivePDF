57d524f71cf129d8a6f82c881b6e30f5f2343807
anisite <infologique@gmail.com>
2020-03-22 14:30:09 -0400

    Ajoutez des fichiers projet.

---
+++ ReceivePDF/Controllers/PdfController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ReceivePDF.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PdfController : ControllerBase
+    {
+
+        private readonly ILogger<PdfController> _logger;
+
+        public PdfController(ILogger<PdfController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<string>> PostAsync()
+        {
+            string pdf;
+            using (StreamReader reader = new StreamReader(Request.Body, Encoding.UTF8))
+            {
+                pdf = await reader.ReadToEndAsync();
+            }
+              
+
+            return "Good";
+        }
+    }
+}
